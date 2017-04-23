9999608d8b6914eac224a40a7c95729339400541
DrIbrahimAli <dr.ibrahim.nofel@gmail.com>
2017-04-23 11:21:38 +0100

    fixed mail server credentials

--- OWL/settings.py
+++ OWL/settings.py
@@ -125,6 +125,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'account@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
