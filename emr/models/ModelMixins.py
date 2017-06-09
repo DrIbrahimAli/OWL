6c370056f7cd73cb189bee6894e6489d3dc77b3f
DrIbrahimAli <dr.ibrahim.nofel@gmail.com>
2017-06-09 17:29:42 +0200

    patient age last_admission ... etc are now properties added age to retreived history :)

--- emr/models/ModelMixins.py
+++ emr/models/ModelMixins.py
@@ -17,86 +17,94 @@ class PatientMethods(object):
         return '/emr/patient/{}'.format(self.id)
 
 
+    @property
     def blood_group(self):
         if self.get_abo_blood_display() and self.get_rh_blood_display():
             return '{} {}'.format(self.get_abo_blood_display(), self.get_rh_blood_display())
         else:
             return 'blood group not known'
 
+    @property
     def never_admitted(self):
         if self.admission_set.exists():
             return False
         else:
             return True
 
-
+    @property
     def last_admission(self):
         return self.admission_set.latest('pk')
 
+    @property
     def age_on_admission(self, *args):
         try :
             admission_pk=args[0]
         except:
-            admission_pk=self.last_admission().pk
+            admission_pk=self.last_admission.pk
         return self.admission_set.get(pk=admission_pk).date.year - self.date_of_birth.year
 
-    def age(self):
+   @property
+   def age(self):
         if self.date_of_birth:
-            if self.never_admitted():
+            if self.never_admitted:
                 return date.today().year - self.date_of_birth.year
             else:
-                return self.age_on_admission()
+                return self.age_on_admission
         else:
             return ' date of birth not known'
 
-    def last_discharge(self):
+   @property
+   def last_discharge(self):
         try:
             return self.discharge_set.latest('pk')
         except:
             return None
 
+    @property
     def los(self):
         try:
-            return str((self.last_admission().discharge.date - self.last_admission().date).days)
+            return str((self.last_admission.discharge.date - self.last_admission.date).days)
         except:
-            return str((date.today() - self.last_admission().date).days)
+            return str((date.today() - self.last_admission.date).days)
 
-
+    @property
     def is_currently_admitted(self):
-        if not self.never_admitted():
+        if not self.never_admitted:
             try:
-                self.last_admission().discharge
+                self.last_admission.discharge
                 return False
             except:
                 return True
         else:
             return False
 
+    @property
     def has_been_discharged(self):
-        if not self.never_admitted():
-            return not self.is_currently_admitted()
+        if not self.never_admitted:
+            return not self.is_currently_admitted
         else:
             return False
 
+    @property
     def is_deceased(self):
-        if self.never_admitted():
+        if self.never_admitted:
             return False
-        elif self.is_currently_admitted():
+        elif self.is_currently_admitted:
             return False
         else:
             return self.last_discharge.mortality
 
 
     def admit(self, *args, **kwargs):
-        if self.is_currently_admitted():
+        if self.is_currently_admitted:
             print('patient is already admitted')
         else:
             print('admitting '+ str(self))
             self.admission_set.create(*args, **kwargs)
 
     def discharge(self,*args, **kwargs):
-        if self.is_currently_admitted():
-            kwargs['admission'] = kwargs.get('admission',self.last_admission())
+        if self.is_currently_admitted:
+            kwargs['admission'] = kwargs.get('admission',self.last_admission)
             try:
                 self.discharge_set.create(*args, **kwargs)
                 print('discharging {}'.format(self))
@@ -108,7 +116,7 @@ class PatientMethods(object):
     def get_highlights(self):
         quotes=[]
         highlights=''
-        for note in self.last_admission().note_set.all():
+        for note in self.last_admission.note_set.all():
             interest = re.compile('#.*?#').findall(note.status)
             interest += re.compile('#.*?#').findall(note.plan)
             interest += re.compile('#.*?#').findall(note.events)
@@ -117,7 +125,7 @@ class PatientMethods(object):
         quotes.sort()
         for quote in quotes:
             highlights +='\n\n\t{}'.format('\n'.join(quote))
-        return format_html(highlights)
+        return highlights
 
     def get_labs(self):
         try:
@@ -151,7 +159,7 @@ class PatientMethods(object):
             risk += 'dyslipidemic; '
         if history.is_smoker:
             risk += 'smoker '
-        risk += self.get_sex_display() + ' patient.'
+        risk += self.age + self.get_sex_display() + ' patient.'
         return risk
 
     def get_present_history(self):
